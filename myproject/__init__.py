299343aeee677fdd5e61cd036b9d55f7023fa6b8
josekang <josekangethe2@gmail.com>
2019-09-18 19:31:01 +0300

    Trying to resolve the issue of internal server error

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -17,19 +17,31 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SECRET_KEY"] = "@#$!@#$%^&*SERTTYBHKJBHFC!@#$%^&*()*&^<%1 id=@QWESDFXCGBHJNJKNBVCSRETYU</%1>"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config['MAIL_USE_TLS'] = False
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "arigabrian.5@gmail.com"
-app.config["MAIL_PASSWORD"] = "857036bc"
+# app.config["MAIL_SERVER"] = "smtp.gmail.com"
+# app.config["MAIL_PORT"] = 465
+# app.config['MAIL_USE_TLS'] = False
+# app.config["MAIL_USE_SSL"] = True
+# app.config["MAIL_USERNAME"] = "arigabrian.5@gmail.com"
+# app.config["MAIL_PASSWORD"] = "857036bc"
+
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = "arigabrian.5@gmail.com",
+    MAIL_PASSWORD = "857036bc",
+))
+
+mail = Mail(app)
 
 
 db = SQLAlchemy(app)
 
 Migrate(app, db)
 
-mail.__init__(app)
+# mail.__init__(app)
 
 login_manager.__init__(app)
 login_manager.login_view = "login"
